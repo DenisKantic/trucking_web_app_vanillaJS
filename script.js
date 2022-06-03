ca3ba00e54322b401998dddf9696230264162041
aalicic <adnan@leads.ba>
2022-06-03 10:47:36 +0200

    new commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let docReq = new XMLHttpRequest();
+
+docReq.open("GET", "data.json", true);
+
+docReq.onload = function () {
+  if (docReq.status == 200) {
+    let tableAppendData = document.getElementsByClassName("table-data");
+
+    let tableData = JSON.parse(docReq.responseText);
+
+    for (let i = 0; i < tableData.length; i++) {
+      let insertTable = document.createElement("tr");
+      insertTable.classList = "inserted-data-table";
+      insertTable.innerHTML =
+        `<td>${tableData[i].Age + "m"}` +
+        `<td>${tableData[i].PickupDate}` +
+        `<td>${tableData[i].Origin}` +
+        `<td>${tableData[i].Destination}` +
+        `<td>${tableData[i].Broker}` +
+        `<td>${tableData[i].CS}` +
+        `<td>${tableData[i].DTP}` +
+        `<td>${tableData[i].VehicleType}`;
+
+      tableAppendData[0].append(insertTable);
+    }
+  } else {
+    alert("document not inserted");
+  }
+};
+
+docReq.send();
